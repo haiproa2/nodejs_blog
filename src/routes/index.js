1ea3a88ed3ec28cddbea60936c2229215ff8204b
Minh Hai <minhhai.dw@gmail.com>
2022-09-05 21:29:11 +0700

    modifine router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
+const express = require('express');
 const newsRouter = require('./news');
-const siteRouter = require('./site');
+
+const siteController = require('../app/controllers/SiteController');
 
 function route(app){
+    // News router use new.js in folder routes
     app.use('/news', newsRouter);
-    app.use('/', siteRouter);
+
+    // Site router
+    app.use('/search', siteController.search);
+    app.use('/', siteController.index);
 }
 
 module.exports = route;
